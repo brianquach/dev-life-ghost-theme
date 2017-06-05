6668aff6e53b429f3f519f0545e15b298bd4bca7
Brian Quach <13rianquach@gmail.com>
2017-06-05 15:45:46 -0700

    Chore: improve first time setup experience

---
+++ bq-theme/scripts/ghost_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+rm -rf ~/bq-theme/ghost # remove existing ghost directory
+
+cd ~
+wget https://ghost.org/zip/ghost-latest.zip
+sudo unzip -uo ghost-latest.zip -d ~/bq-theme/ghost
+rm -rf ghost-latest.zip # clean up
+
+cd ~/bq-theme/ghost && sudo npm install --production
+
+cp config.example.js config.js
+
+# Replace 127.0.0.1 with 0.0.0.0 to give ghost access to host machine
+sed -i "s/127.0.0.1/0.0.0.0/g" config.js
+
+# install forever and run ghost
+npm install -g forever
+forever index.js
---
+++ bq-theme/scripts/node_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm install 4.2
+nvm alias default 4.2
+nvm use default
