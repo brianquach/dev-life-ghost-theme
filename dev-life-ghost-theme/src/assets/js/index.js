4fb484675578823a8186f516a87fe47e2ee384c9
Brian Quach <13rianquach@gmail.com>
2017-06-07 21:54:05 -0700

    Chore: update project to use webpack output

--- dev-life-ghost-theme/src/assets/js/index.js
+++ dev-life-ghost-theme/src/assets/js/index.js
@@ -3,54 +3,58 @@
  */
 
 /* globals jQuery, document */
-(function ($, undefined) {
-    "use strict";
+(function($, undefined) {
+    'use strict';
 
-    var $document = $(document);
+    const $document = $(document);
 
-    $document.ready(function () {
-
-        var $postContent = $(".post-content");
+    $document.ready(function() {
+        const $postContent = $('.post-content');
         $postContent.fitVids();
 
-        $(".scroll-down").arctic_scroll();
+        $('.scroll-down').arctic_scroll();
 
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
+        $('.menu-button, .nav-cover, .nav-close').on('click', function(e) {
             e.preventDefault();
-            $("body").toggleClass("nav-opened nav-closed");
+            $('body').toggleClass('nav-opened nav-closed');
         });
-
     });
 
     // Arctic Scroll by Paul Adam Davis
     // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
+    $.fn.arctic_scroll = function(options) {
+        const defaults = {
+            elem: this,
+            $elem: $(this),
+            speed: 500,
+        };
+        const allOptions = $.extend(defaults, options);
 
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
-
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
+        allOptions.elem.click(function(event) {
+            const el = defaults.elem;
+            const $this = defaults.$elem;
+            const $htmlBody = $('html, body');
+            const offset = ($this.attr('data-offset'))
+                ? $this.attr('data-offset')
+                : false;
+            const position = ($this.attr('data-position'))
+                ? $this.attr('data-position')
+                : false;
+            let toMove;
 
             if (offset) {
                 toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+                $htmlBody.stop(true, false).animate(
+                  {scrollTop: ($(el.hash).offset().top + toMove)},
+                  allOptions.speed);
             } else if (position) {
                 toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+                $htmlBody.stop(true, false).animate(
+                  {scrollTop: toMove}, allOptions.speed);
             } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+                $htmlBody.stop(true, false).animate(
+                  {scrollTop: ($(el.hash).offset().top)}, allOptions.speed);
             }
         });
-
-    };
+      };
 })(jQuery);
--- dev-life-ghost-theme/src/assets/js/webpack.js
+++ dev-life-ghost-theme/src/assets/js/webpack.js
@@ -2,5 +2,4 @@
 import '../css/screen.css';
 
 // Load default scripts
-import './jquery.fitvids.js';
 import './index.js';
