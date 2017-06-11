a18d37c074c9d7d139645bb66ac9c76be93d5626
Brian Quach <13rianquach@gmail.com>
2017-06-11 15:41:23 -0700

    Config: always use autoprefixer

--- dev-life-ghost-theme/postcss.config.js
+++ dev-life-ghost-theme/postcss.config.js
@@ -3,7 +3,7 @@ module.exports = ({ file, options, env }) => ({
   plugins: {
     'postcss-import': { root: file.dirname },
     'postcss-cssnext': options.cssnext ? options.cssnext : false,
-    'autoprefixer': env == 'production' ?  : false,
+    'autoprefixer': options.autoprefixer,
     'cssnano': env === 'production' ? options.cssnano : false
   }
 });
